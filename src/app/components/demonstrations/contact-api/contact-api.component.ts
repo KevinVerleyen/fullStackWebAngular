8dcf71ad2d57cfe2aa9bbc3bf820abd080abda42
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-03-05 16:19:04 +0100

    fullstack angular exo

--- src/app/components/demonstrations/contact-api/contact-api.component.ts
+++ src/app/components/demonstrations/contact-api/contact-api.component.ts
@@ -3,66 +3,89 @@ import { Post } from '../../../models/post.model';
 import { ContactApiDemoService } from '../../../tools/services/Api/contact-api-demo.service';
 
 @Component({
-  selector: 'app-contact-api',
-  standalone: false,
+    selector: 'app-contact-api',
+    standalone: false,
 
-  templateUrl: './contact-api.component.html',
-  styleUrl: './contact-api.component.scss'
+    templateUrl: './contact-api.component.html',
+    styleUrl: './contact-api.component.scss',
 })
 export class ContactApiComponent {
-
-    posts : Post[] = [];
-    postModifie : Post = {
-        id : 1,
-        title : "",
-        body : "",
-        userId : 1
+    posts: Post[] = [];
+    postModifie: Post = {
+        id: 1,
+        title: '',
+        body: '',
+        userId: 1,
     };
-    messageErreur : string = "";
-    etaModif : boolean = false;
+    messageErreur: string = '';
+    etaModif: boolean = false;
 
-    nouveauPost : Post = {
+    nouveauPost: Post = {
         title: 'test',
-        body : "Toto tata titi",
-        userId : 1
+        body: 'Toto tata titi',
+        userId: 1,
     };
+    selectedPost: any;
 
-    constructor(private _contactApi : ContactApiDemoService){
-
-    }
+    constructor(private _contactApi: ContactApiDemoService) {}
 
     ngOnInit(): void {
         this._contactApi.RecupererPosts().subscribe({
-            next : (data) => {
-                this.posts = data.slice(0,10);
+            next: (data) => {
+                this.posts = data.slice(0, 10);
             },
-            error : (error) =>{
+            error: (error) => {
                 this.messageErreur = error.message;
             },
-            complete : () => {
-                console.log("Récupération terminée....");
-            }
-        })
+            complete: () => {
+                console.log('Récupération terminée....');
+            },
+        });
     }
 
-    etatModif() : void {
+    etatModif(): void {
         this.etaModif = true;
     }
 
-    modifPost() : void {
+    modifPost(): void {
         this._contactApi.ModifPost(this.postModifie).subscribe({
-            next : (data) => {
-                const index = this.posts.findIndex((post) => post.id === data.id);
-                    if (index !== -1) {
-                        this.posts[index] = data;
-                        this.postModifie.title = "";
-                        this.postModifie.body = "";
-                        this.etaModif = false;
-                    }
+            next: (data) => {
+                const index = this.posts.findIndex(
+                    (post) => post.id === data.id
+                );
+                if (index !== -1) {
+                    this.posts[index] = data;
+                    this.postModifie.title = '';
+                    this.postModifie.body = '';
+                    this.etaModif = false;
+                }
             },
-            error : (error) =>{
+            error: (error) => {
                 console.log(error.message);
-            }
-        })
+            },
+        });
+    }
+    AjouterPost(): void {
+        this._contactApi.AjouterPost(this.nouveauPost).subscribe({
+            next: (data) => {
+                this.posts.unshift(data);
+            },
+            error: (error) => {
+                console.log(error.message);
+            },
+        });
+    }
+
+    SupprimerPost(id: number | undefined): void {
+        if (id) {
+            this._contactApi.SupprimerPost(id).subscribe({
+                next: () => {
+                    this.posts = this.posts.filter((post) => post.id !== id);
+                },
+                error: (error) => {
+                    console.log(error.message);
+                },
+            });
+        }
     }
 }
--- src/app/tools/services/Api/contact-api-demo.service.ts
+++ src/app/tools/services/Api/contact-api-demo.service.ts
@@ -4,28 +4,34 @@ import { Observable } from 'rxjs';
 import { Post } from '../../../models/post.model';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class ContactApiDemoService {
-    private _apiUrl : string = "https://jsonplaceholder.typicode.com/posts";
+    private _apiUrl: string = 'https://jsonplaceholder.typicode.com/posts';
 
-    constructor(private _httpClient: HttpClient) { }
+    constructor(private _httpClient: HttpClient) {}
 
     // CRUD
 
     // récupérer tout les posts
-    RecupererPosts() : Observable<Post[]> {
+    RecupererPosts(): Observable<Post[]> {
         return this._httpClient.get<Post[]>(this._apiUrl);
     }
 
     // ajouter un post
+    AjouterPost(post: Post): Observable<Post> {
+        return this._httpClient.post<Post>(this._apiUrl, post);
+    }
 
     // Modiffier un post
-    ModifPost(post : Post) : Observable<Post>{
+    ModifPost(post: Post): Observable<Post> {
         console.log(post);
-        
-        return this._httpClient.put<Post>(`${this._apiUrl}/${post.id}`, post)
+
+        return this._httpClient.put<Post>(`${this._apiUrl}/${post.id}`, post);
     }
 
     // Supprimer un post
+    SupprimerPost(id: number): Observable<void> {
+        return this._httpClient.delete<void>(`${this._apiUrl}/${id}`);
+    }
 }
